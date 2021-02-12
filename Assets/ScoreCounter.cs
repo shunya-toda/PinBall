eb0eb44d5288e03f7d164e78528781d86e42cd3e
shunya-toda <77889501+shunya-toda@users.noreply.github.com>
2021-02-13 03:15:20 +0900

    Initial commit

---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class ScoreCounter : MonoBehaviour
+{
+    private GameObject scoreText;
+
+    private int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetScore();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        
+        SetScore();
+        string tag = other.gameObject.tag;
+
+        if (tag == "SmallStarTag")
+        {
+            score += 5;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            score += 15;
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            score += 10;
+        }
+        else if (tag == "LargeCloudTag")
+        {
+            score += 20;
+        }
+
+    }
+    void SetScore()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+
+        scoreText.GetComponent<Text>().text = score.ToString();
+    }
+}
